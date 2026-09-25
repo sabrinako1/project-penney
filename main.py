5a1dff6ac70565b88b8634269de48f63359eaf58
sabrinako1 <sko@wm.edu>
2026-09-25 10:45:50 -0400

    Adding visualization

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from src.generator import Generator
 import matplotlib.pyplot as plt
+from src.viz import visualize
 
-gen = Generator(simulations=100_000)
+simulations = 10_000
+
+gen = Generator(simulations)
 
 p1_wins = gen.run()
-
-# B = 0, R = 1. First suit is 4x, second suit is 2x, third suit is 1x.
-# BBB -> 0, BRB -> 2, BBR -> 1, RRR -> 7
-combinations = ['BBB', 'BBR', 'BRB', 'BRR', 'RBB', 'RBR', 'RRB', 'RRR']
-
-fig, ax = plt.subplots(figsize=(9, 7))
-heatmap = ax.imshow(p1_wins)
-
-ax.set_xticks(range(len(combinations)), labels=combinations)
-ax.set_yticks(range(len(combinations)), labels=combinations)
-
-plt.show()
+visualize(p1_wins,simulations)
---
+++ src/viz.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+def visualize(p1_wins, num_decks):
+
+# SCORING BY CARDS
+    cards_scores = p1_wins
+    data = cards_scores
+    cards_scores[cards_scores == 0] = -1
+    cards_combos = ["BBB","BBR","BRB","BRR","RBB","RBR","RRB","RRR"]
+
+    
+    plt.subplot(1, 2, 1)
+    viz = sns.heatmap(data, annot=True,xticklabels = cards_combos, yticklabels=cards_combos,cbar=False)
+    plt.ylabel("Opponent Choice", fontsize = 12, labelpad=12)
+    plt.xlabel("My Choice", fontsize = 12, labelpad = 12)
+    plt.title(f"My Probability of Win(Tie) Scoring By Cards \n N = {num_decks}")
+
+    # SCORING BY TRICKS
+    tricks_scores = np.random.randint(1, 10,(8,8))
+    data = tricks_scores
+    tricks_scores[tricks_scores == 0] = -1
+    cards_combos = ["BBB","BBR","BRB","BRR","RBB","RBR","RRB","RRR"]
+
+    plt.subplot(1, 2, 2)
+    viz = sns.heatmap(data, xticklabels = cards_combos, yticklabels=cards_combos,cbar=False)
+    plt.ylabel("Opponent Choice", fontsize = 12, labelpad = 12)
+    plt.xlabel("My Choice", fontsize = 12, labelpad = 12)
+    plt.title(f"My Probability of Win(Tie) Scoring By Tricks \n N = {num_decks}")
+    plt.tight_layout()
+    plt.show()
