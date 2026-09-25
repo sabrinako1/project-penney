354ab9ae2b434232cef87e68ec5c63d3b9bd012a
sabrinako1 <sko@wm.edu>
2026-09-25 11:38:59 -0400

    Visualization

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.generator import Generator
 import matplotlib.pyplot as plt
 from src.viz import visualize
 
-simulations = 10_000
+simulations = 10_00
 
 gen = Generator(simulations)
 
 p1_wins_by_trick, p1_ties_by_trick, p1_wins_by_card, p1_ties_by_card  = gen.run()
-visualize(p1_wins_by_trick, simulations)
+visualize(p1_wins_by_trick, p1_ties_by_trick, p1_wins_by_card, p1_ties_by_card, simulations)
--- src/viz.py
+++ src/viz.py
@@ -2,10 +2,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 
-def visualize(p1_wins, num_decks):
+def visualize(p1_wins_by_trick, p1_ties_by_trick, p1_wins_by_card, p1_ties_by_card, num_decks):
 
 # SCORING BY CARDS
-    cards_scores = p1_wins
+    cards_scores = (p1_wins_by_card/num_decks) * 100
     data = cards_scores
     cards_scores[cards_scores == 0] = -1
     cards_combos = ["BBB","BBR","BRB","BRR","RBB","RBR","RRB","RRR"]
@@ -16,13 +16,13 @@ def visualize(p1_wins, num_decks):
     plt.title(f"My Probability of Win(Tie) Scoring By Cards \n N = {num_decks}")
 
     # SCORING BY TRICKS
-    tricks_scores = np.random.randint(1, 10,(8,8))
+    tricks_scores = (p1_wins_by_trick/num_decks) * 100
     data = tricks_scores
     tricks_scores[tricks_scores == 0] = -1
     cards_combos = ["BBB","BBR","BRB","BRR","RBB","RBR","RRB","RRR"]
 
     plt.subplot(1, 2, 2)
-    viz = sns.heatmap(data, xticklabels = cards_combos, yticklabels=cards_combos,cbar=False)
+    viz = sns.heatmap(data, annot=True, xticklabels = cards_combos, yticklabels=cards_combos,cbar=False)
     plt.ylabel("Opponent Choice", fontsize = 12, labelpad = 12)
     plt.xlabel("My Choice", fontsize = 12, labelpad = 12)
     plt.title(f"My Probability of Win(Tie) Scoring By Tricks \n N = {num_decks}")
