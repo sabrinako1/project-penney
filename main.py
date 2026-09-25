d8c09801d66fe596c92193edb7b88c85d342924b
timmyc181 <timothy.j.cleveland1@gmail.com>
2026-09-25 11:09:45 -0400

    Scoring.

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ simulations = 10_000
 
 gen = Generator(simulations)
 
-p1_wins = gen.run()
-visualize(p1_wins,simulations)
+p1_wins_by_trick, p1_ties_by_trick, p1_wins_by_card, p1_ties_by_card  = gen.run()
+visualize(p1_wins_by_trick, simulations)
